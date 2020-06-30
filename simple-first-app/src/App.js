f2a72f9778a4bea4a4b2ff2020605b267276207f
anshulmandrelia <anshulmandrelia2000@gmail.com>
2020-06-30 22:22:40 +0530

    fixed

--- simple-first-app/src/App.js
+++ simple-first-app/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello World</h1>
-    );
+    // return (
+    //   <h1>Hello World</h1>
+    // );
+    return React.createElement('div','h1','Hi Im react app')
+
   }
 }
 
